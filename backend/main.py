50dc9258acb9425754f03ca400cace0dc0d957c0
Derek Johnson <146795367+derekjohnson7@users.noreply.github.com>
2026-08-22 21:54:40 -0500

    Fix formatting of CORS middleware origins

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-		"https://backyardos-dash.onrender.com/"
+		"https://backyardos-dash.onrender.com/",
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "OPTIONS"],
